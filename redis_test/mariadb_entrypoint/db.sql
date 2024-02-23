5ae00ba2a0ec09997be48bd0a02ad92c34b2fb63
rahul@PC <tanwar.r@gmail.com>
2024-02-23 08:17:02 +0530

    added startup script mariadb

---
+++ redis_test/mariadb_entrypoint/db.sql
@@ -0,0 +1,9 @@
+create database exampledb;
+CREATE TABLE your_table(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100)
+);
+INSERT INTO your_table(name, email) VALUES
+    ('John Doe', 'john@example.com'),
+    ('Jane Smith', 'jane@example.com');
