6a7c234014aa2c7695705d1364ed47e51d24cdeb
rahul@PC <tanwar.r@gmail.com>
2024-02-25 17:30:05 +0530

    added sample db and slow queries

--- redis_test/app.py
+++ redis_test/app.py
@@ -3,19 +3,58 @@ import mysql.connector
 import redis
 import logging
 import sys
+import random
+import string
 
 app = Flask(__name__)
+#engine = db.create_engine('mysql+pymysql://exampleuser:examplepassword@mariadb/exampledb?charset=utf8mb4')
+
 logging.basicConfig(level=logging.DEBUG)
 # Connect to MariaDB
-db_connection = mysql.connector.connect(
-    host='mariadb',
-    user='exampleuser',
-    password='examplepassword',
-    database='exampledb'
-)
+#db_connection = mysql.connector.connect( host='mariadb', user='exampleuser', password='examplepassword', database='exampledb', connect_timeout=28800 )
+
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'mariadb'
+app.config['MYSQL_USER'] = 'exampleuser'
+app.config['MYSQL_PASSWORD'] = 'examplepassword'
+app.config['MYSQL_DB'] = 'classicmodels'
+mysql = MySQL(app)
+
+
+def generate_random_string(length):
+    return ''.join(random.choices(string.ascii_letters, k=length))
+
+# Function to generate dummy data
+
+def generate_dummy_data(num_rows):
+    dummy_data = []
+    for _ in range(num_rows):
+        name = generate_random_string(10)
+        email = f"{name}@example.com"
+        dummy_data.append((name, email))
+    return dummy_data
+
+# Function to insert dummy data into the database
+@app.route('/dummydata/<int:num_rows>')
+def insert_dummy_data(num_rows):    
+    #c = db_connection.cursor()
+    c = mysql.connection.cursor()
+    dummy_data = generate_dummy_data(num_rows)  # Generating 10 rows of dummy data
+    print(dummy_data)
+    c.executemany('INSERT INTO your_table (name, email) VALUES (%s, %s)', dummy_data)
+    #db_connection.commit()
+    mysql.connection.commit()
+    return f'{num_rows} Record(s) Inserted!'
+    
 
 # Connect to Redis
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+
+
+
 def redis_cache_info():
     hits = redis_client.info()['keyspace_hits']
     # Get the number of failed lookups of keys
@@ -23,67 +62,81 @@ def redis_cache_info():
     # Calculate and print the hit/miss ratio
     if hits + misses > 0:
         ratio = hits / (hits + misses)
-        return f'Hit/Miss ratio: {ratio}'
+        return f'Hit/Miss ratio: {ratio} {hits}/{misses}'
     else:
         return 'No data yet'
 
 
 def try_cached_data(qry,parameters=None):
-    query_key  = f"{qry}|{parameters}"
-    cached_data = redis_client.get(query_key )
-    if cached_data:
-        data = cached_data.decode('utf-8')        
+    cached_data=None
+    if parameters:
+        query_key  = f"{qry}|{parameters}"
     else:
-        cursor = db_connection.cursor()
+        query_key  = f"{qry}"    
+    #fetch data from cache
+    cached_data = redis_client.get(query_key)    
+    if cached_data:
+        print('from CACHE')
+        data = cached_data.decode('utf-8')
+    else:
+        print('from DB')
+        #cursor = db_connection.cursor(buffered=True)
+        cursor = mysql.connection.cursor()
         if parameters:
             cursor.execute(qry,parameters)
         else:
             cursor.execute(qry)
-        data = cursor.fetchall()
+        data = cursor.fetchall()        
+        redis_client.set(query_key, str(data))
         cursor.close()    
-        redis_client.set(qry, str(data))
     return jsonify({'data': data})
+
+
+def get_db_data(qry,parameters=None):    
+    print('from DB')
+    cursor = mysql.connection.cursor()
+    if parameters:
+        cursor.execute(qry,parameters)
+    else:
+        cursor.execute(qry)
+    data = cursor.fetchall()
+    #redis_client.set(query_key, str(data))
+    cursor.close()    
+    return jsonify({'data': data})
+
+
 
 # Sample route to fetch data from MariaDB with Redis caching
 @app.route('/data')
 @app.route('/data/<id>')
-@app.route('//<id>')
-@app.route('/')
 def get_data(id=None):
     import random
     #num = random.randint(0,2)
     print(id)    
     if (id):
-        qry = 'SELECT * FROM your_table  WHERE id=%s'
+        qry = 'SELECT customerNumber,customerName,SLEEP(0.01) sleep FROM customers where customerNumber=%s'        
         data = try_cached_data(qry,(id,))
     else:
-        qry = 'SELECT * FROM your_table'
+        qry = 'SELECT customerNumber,customerName,SLEEP(0.01) sleep FROM customers'
         data = try_cached_data(qry)
-    #cache_hits = redis_client.get('keyspace_hits')
-    #cache_misses = redis_client.get('keyspace_misses')
-    #print("Cache hits:", int(cache_hits) if cache_hits else 0)
-    #print("Cache misses:", int(cache_misses) if cache_misses else 0) 
     print(redis_cache_info())
-    #print(redis_client.info())
-    #app.logger.warning('testing warning log')
-    #app.logger.error('testing error log')
-    #app.logger.info('testing info log')
-    
-    #cached_data = redis_client.get(qry,input)    
-    #if cached_data:
-    #    print('cached_data')
-    #return jsonify({'data': data.decode('utf-8')})
     return data
-        #return jsonify({'data': cached_data.decode('utf-8')},{'redis_client':redis_client.info()} )
-    # If data not found in cache, fetch from MariaDB
-    cursor = db_connection.cursor()    
-    cursor.execute(qry,input)
-    data = cursor.fetchall()
-    cursor.close()
-    # Cache the fetched data in Redis
-    redis_client.set(qry, str(data))
-    return jsonify({'data': data})
-    #return "Check your console"
+
+@app.route('/dbdata')
+@app.route('/dbdata/<id>')
+def get_dbdata(id=None):
+    import random
+    #num = random.randint(0,2)
+    print(id)    
+    if (id):
+        qry = 'SELECT customerNumber,customerName,SLEEP(0.01) sleep FROM customers where customerNumber=%s'        
+        data = get_db_data(qry,(id,))
+    else:
+        qry = 'SELECT customerNumber,customerName,SLEEP(0.01) sleep FROM customers'
+        data = get_db_data(qry)    
+    return data
+
+
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- redis_test/mariadb_entrypoint/db.sql
+++
@@ -1,9 +0,0 @@
---create database exampledb;
-CREATE TABLE your_table(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    email VARCHAR(100)
-);
-INSERT INTO your_table(name, email) VALUES
-    ('John Doe', 'john@example.com'),
-    ('Jane Smith', 'jane@example.com');
