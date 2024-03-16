84415a3b72a7cf0237db4d5f1337c084287e6ced
rahul@PC <tanwar.r@gmail.com>
2024-03-16 17:47:03 +0530

    insert records in sqlite db

--- rabbitmq/Consumer/consumer.py
+++ rabbitmq/Consumer/consumer.py
@@ -3,30 +3,85 @@ import os
 import sys
 import time
 import random
+import sqlite3
+
 
 rabbit_host = os.environ.get('rabbitmq_host', 'localhost')
 rabbit_port = os.environ.get('rabbitmq_port', '5672')
 rabbit_queue_name = os.environ.get('rabbitmq_queue', 'queue')
-connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port))
-channel = connection.channel()
+rabbit_pass = os.environ.get('RABBITMQ_DEFAULT_PASS', 'guest')
+rabbit_user = os.environ.get('RABBITMQ_DEFAULT_USER', 'guest')
+credentials = pika.PlainCredentials(rabbit_user,rabbit_pass)
+#connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port,"/",credentials))
+#channel = connection.channel()
+
+database_path = "/db/example.db"
+
 
 def callback(ch, method, properties, body):
     print(f" [x] Received {body}")
 
 
+def create_connection(database):
+    """Create a database connection to the SQLite database specified by the database file."""
+    db_connection = None
+    try:
+        db_connection = sqlite3.connect(database)
+        print("Connection to SQLite database successful")
+        return db_connection
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+
+db_connection = create_connection(database_path)
+table_name = "example_table"
+
+def read_row_by_id(db_connection, table_name, row_id):
+    """Read a specific row from a table by ID."""
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute(f"SELECT * FROM {table_name} WHERE id=?", (row_id,))
+        row = cursor.fetchone()
+        if row:
+            print(f"Data for row with ID {row_id} in table '{table_name}':")
+            return(row)
+        else:
+            print(f"No data found for row with ID {row_id} in table '{table_name}'")
+            return None
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+def update_row_by_id(connection, table_name, row_id, new_data):
+    """Update a specific row in a table by ID."""
+    try:
+        cursor = connection.cursor()
+        update_query = f"""
+        UPDATE {table_name}
+        SET sum = ?            
+        WHERE id = ?
+        """
+        cursor.execute(update_query, (*new_data, row_id))
+        connection.commit()
+        print(f"Row with ID {row_id} updated successfully")
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port,"/",credentials))
     channel = connection.channel()
 
     channel.queue_declare(queue=rabbit_queue_name,durable=True)
 
     def callback(ch, method, properties, body):
         #print(f" [x] Received {body}")
-        print(f" [x] Received {body.decode()}")
+        row_id_to_read = body.decode()
+        print(f" [x] Received {row_id_to_read}")
         time_taken=random.randint(1, 3)
         time.sleep(time_taken)
-
+        values = read_row_by_id(db_connection, table_name, row_id_to_read)
+        sum = values[0]+values[1]
+        update_row_by_id(db_connection,table_name,row_id_to_read,(sum,))
         print(f" [x] Done in {time_taken} Second(s).")
         ch.basic_ack(delivery_tag=method.delivery_tag)
     channel.basic_qos(prefetch_count=1)
--- rabbitmq/Publisher/publisher.py
+++ rabbitmq/Publisher/publisher.py
@@ -1,12 +1,65 @@
 import pika
 import os
 import sys
+import sqlite3
+import random
 
 rabbit_host = os.environ.get('rabbitmq_host', 'localhost')
 rabbit_port = os.environ.get('rabbitmq_port', '5672')
 rabbit_queue_name = os.environ.get('rabbitmq_queue', 'queue')
-connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port))
+rabbit_pass = os.environ.get('RABBITMQ_DEFAULT_PASS', 'guest')
+rabbit_user = os.environ.get('RABBITMQ_DEFAULT_USER', 'guest')
+credentials = pika.PlainCredentials(rabbit_user,rabbit_pass)
+connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host,rabbit_port,"/",credentials))
 channel = connection.channel()
+    # Create a connection to the database
+
+
+def create_connection(database):
+    """Create a database connection to the SQLite database specified by the database file."""
+    db_connection = None
+    try:
+        db_connection = sqlite3.connect(database)
+        print("Connection to SQLite database successful")
+        return db_connection
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+def create_table(db_connection, create_table_sql):
+    """Create a table from the create_table_sql statement."""
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute(create_table_sql)
+        print("Table created successfully")
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+def insert_data(db_connection, insert_data_sql, data):
+    """Insert data into the table."""
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute(insert_data_sql, data)
+        db_connection.commit()        
+        print("Data inserted successfully")
+        return cursor.lastrowid
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+database_path = "/db/example.db"
+
+    # SQL statements
+create_table_sql = """
+    CREATE TABLE IF NOT EXISTS example_table (
+        id INTEGER PRIMARY KEY,
+        value1 INTEGER,
+        value2 INTEGER,
+        sum INTEGER
+    );
+    """
+
+insert_data_sql = """
+    INSERT INTO example_table (value1, value2) VALUES (?, ?)
+    """
 
 def send_message(channel,rabbit_queue_name,message):
     
@@ -14,15 +67,30 @@ def send_message(channel,rabbit_queue_name,message):
     properties=pika.BasicProperties(
                      delivery_mode=2#pika.DeliveryMode.Persistent
                         )
-    channel.basic_publish(exchange='',
-                      routing_key=rabbit_queue_name,properties=properties,body=message)
     
+    if db_connection is not None:
+    # Create table
+        create_table(db_connection, create_table_sql)
+    # Insert data
+        value1=random.randint(1, 999)
+        value2=random.randint(1, 999)
+
+        message=insert_data(db_connection, insert_data_sql, (value1,value2))
+        channel.basic_publish(exchange='', routing_key=rabbit_queue_name,properties=properties,body=str(message))
+    # Close connection
+        db_connection.close()
+    else:
+        print("Unable to establish connection to the database")
     
     print(f" [x] Sent {message}")
 
 message = 'Hello World!'
 if len(sys.argv) > 1:
     message = sys.argv[1]
-send_message(channel,rabbit_queue_name,message)
 
+db_connection = create_connection(database_path)
+send_message(channel,rabbit_queue_name,message)
 connection.close()
+
+
+
